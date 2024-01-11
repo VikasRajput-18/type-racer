8013f47d614af764e7656c6157925847884f4511
vikasrajput18 <vikasvikas98809@gmail.com>
2024-01-11 10:00:20 +0530

    nits

--- client/src/SocketContext.jsx
+++ client/src/SocketContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 import io from "socket.io-client";
 
-let backend_url = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+let backend_url = import.meta.env.VITE_BACKEND_URL || "";
 const socket = io(backend_url, {
   transports: ["websocket"],
 });
