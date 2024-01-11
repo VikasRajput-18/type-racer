cc71154c37ded7952f318a4e59e597197a5ad33e
vikasrajput18 <vikasvikas98809@gmail.com>
2024-01-11 10:08:09 +0530

    cmnt code

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,9 +18,9 @@ const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+// });
 
 const io = new Server(server, { cors: corsOptions });
 
